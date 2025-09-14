46fed5460ea418cc5fac0906fd88dbd8379084f2
ziseilt <1157468223@qq.com>
2025-09-14 12:39:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-weight_input = input("请输入重量（例如：10kg 或 5pd）: ").strip().lower()
-
-# 提取数值和单位
-if weight_input.endswith('kg'):
-    kg = float(weight_input[:-2])
-    pd = kg * 2.2046
-    print(f"{kg}千克 = {pd:.3f}磅")
-elif weight_input.endswith('pd'):
-    pd = float(weight_input[:-2])
-    kg = pd / 2.2046
-    print(f"{pd}磅 = {kg:.3f}千克")
+WeightStr = input("请输入带有符号的重量值：")
+if WeightStr[-2:] == 'kg':
+    pd = eval(WeightStr[:-2]) * 2.2046
+    print("转换后的重量是{:.3f}pd".format(pd))
+elif WeightStr[-2:] == 'pd':
+    kg = eval(WeightStr[:-2]) / 2.2046
+    print("转换后的重量是{:.3f}kg".format(kg))
 else:
-    print("输入格式错误，请使用类似'10kg'或'5pd'的格式")
+    print("输入格式错误")
